321b43c4a04a79332bf51337eca2b971818ad4bd
Santiago Jaramillo <sjara@uoregon.edu>
2026-03-05 15:39:19 -0800

    Added changes to milestones doc and updated version.

--- pyscanbox/__init__.py
+++ pyscanbox/__init__.py
@@ -11,7 +11,7 @@ Main modules:
     gui: PyQt-based graphical user interface (Phase 3)
 """
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 __author__ = "Santiago Jaramillo"
 
 # Import main configuration module
