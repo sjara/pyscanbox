bac6c300b7fe0ff4a80f8fb2a4f85f4950e01794
Santiago Jaramillo <sjara@uoregon.edu>
2026-04-30 17:23:09 -0700

    Set version to 1.8.3+scan_bugfix for branch testing

--- pyscanbox/__init__.py
+++ pyscanbox/__init__.py
@@ -14,7 +14,7 @@ Main modules:
     gui: PyQt-based graphical user interface (Phase 3)
 """
 
-__version__ = "1.8.3"
+__version__ = "1.8.3+scan_bugfix"
 __author__ = "Santiago Jaramillo"
 
 # Import main configuration module
